895bb1e3f440f86c5a02dad67b765afe3d2e7679
DENISE A S SILVA <80422358+alpimdenise@users.noreply.github.com>
2021-12-06 22:47:42 -0300

    delete

--- finonacci.pl
+++
@@ -1,7 +0,0 @@
-## regras  % R = RESULTADO
-fibo(0,R):- R is 0.
-fibo(1,R):- R is 1.
-fibo(N,R):- N > 1,
-                    FIB1 is N-1, FIB2 is N-2,
-                    fibo(FIB1,F1), fibo(FIB2, F2),
-                    R is F1+F2.
